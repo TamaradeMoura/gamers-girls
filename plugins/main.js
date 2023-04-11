98622a9a55e20c46fadbfa90d3cdaa0b5bf7c79e
TamaradeMoura <tamara_moura_13@hotmail.com>
2023-04-10 21:16:25 -0300

    first commit

---
+++ plugins/main.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+    $('#telefone').mask('(+00) 00 00000-0000')
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+            tel: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, insira seu nome',
+            email: 'Por favor, insira seu email',
+            tel: 'Por favor insira seu telefone',
+            mensagem: 'Por favor, insira sua mensagem'
+
+        },
+        submitHandler: function(form) {
+            form.submit();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids()
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`)
+            }
+        }
+    })
+})
